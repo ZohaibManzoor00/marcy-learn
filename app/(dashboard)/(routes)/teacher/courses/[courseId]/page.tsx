01221de089ec0c713fbcba0b7d21cc6e74073e9d
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-03-10 17:56:52 -0400

    reorder list persisting

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -14,6 +14,7 @@ import ImageForm from "./_components/image-form";
 import CategoryForm from "./_components/category-form";
 import PriceForm from "./_components/price-form";
 import AttachmentForm from "./_components/attachment-form";
+import ChaptersForm from "./_components/chapters-form";
 
 export default async function CourseIdPage({
   params,
@@ -24,8 +25,11 @@ export default async function CourseIdPage({
   if (!userId) return redirect("/");
 
   const course = await db.course.findUnique({
-    where: { id: params.courseId },
-    include: { attachments: { orderBy: { createdAt: "desc" } } },
+    where: { id: params.courseId, userId },
+    include: {
+      chapters: { orderBy: { position: "asc" } },
+      attachments: { orderBy: { createdAt: "desc" } },
+    },
   });
 
   const categories = await db.category.findMany({ orderBy: { name: "asc" } });
@@ -38,7 +42,9 @@ export default async function CourseIdPage({
     course.imageUrl,
     course.price,
     course.categoryId,
+    course.chapters.some((chapter) => chapter.isPublished),
   ];
+  
   const totalFields = requiredFields.length;
   const completedFields = requiredFields.filter(Boolean).length;
   const completionText = `${completedFields}/${totalFields}`;
@@ -77,7 +83,7 @@ export default async function CourseIdPage({
               <IconBadge icon={ListChecks} />
               <h2 className="text-xl">Course Chapters</h2>
             </div>
-            <div>TODO: Chapters</div>
+            <ChaptersForm initialData={course} courseId={course.id} />
           </div>
           <div className="flex items-center gap-x-2">
             <IconBadge icon={CircleDollarSign} />
