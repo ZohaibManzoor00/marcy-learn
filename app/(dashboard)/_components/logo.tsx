02f752294ed2f7162459c77a34cd1a956ac784fc
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-03-10 01:08:18 -0500

    first commit

---
+++ app/(dashboard)/_components/logo.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function Logo() {
+  return <Image height={20} width={20} alt="logo" src="/marcy-logo.png" />;
+}
---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Layout, Compass } from "lucide-react";
+import SidebarItem from "./sidebar-items";
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Browse",
+    href: "/search",
+  },
+];
+export default function SidebarRoutes() {
+  const routes = guestRoutes;
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          href={route.href}
+          label={route.label}
+        />
+      ))}
+    </div>
+  );
+}
