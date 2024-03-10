40b557e3d92ac3c4bcdcd6a642bc7c0068904d51
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-03-10 03:03:02 -0400

    create course form

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -4,8 +4,11 @@ import SidebarRoutes from "./sidebar-routes";
 export default function Sidebar() {
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-      <div className="p-6">
+      <div></div>
+      <div className="p-6 flex items-center gap-1">
         <Logo />
+        <p>Marcy</p>
+
       </div>
       <div className="flex flex-col w-full">
         <SidebarRoutes/> </div>
