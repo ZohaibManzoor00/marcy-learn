0077c341a4a822a337ffde350f3c926b9f551d02
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-03-11 17:11:41 -0400

    can delete chapters

--- app/api/courses/[courseId]/chapters/[chapterId]/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/route.ts
@@ -8,11 +8,63 @@ const mux = new Mux({
   tokenSecret: process.env.MUX_TOKEN_SECRET,
 });
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+
+    const ownCourse = await db.course.findUnique({
+      where: { id: params.courseId, userId },
+    });
+
+    if (!ownCourse) return new NextResponse("Unauthorized", { status: 401 });
+
+    const chapter = await db.chapter.findUnique({
+      where: { id: params.chapterId, courseId: params.courseId },
+    });
+
+    if (!chapter) return new NextResponse("Not found", { status: 404 });
+
+    if (chapter.videoUrl) {
+      const existingMuxData = await db.muxData.findFirst({
+        where: { chapterId: params.chapterId },
+      });
+
+      if (existingMuxData) {
+        await mux.video.assets.delete(existingMuxData.assetId);
+        await db.muxData.delete({ where: { id: existingMuxData.id } });
+      }
+    }
+
+    const deletedChapter = await db.chapter.delete({
+      where: { id: params.chapterId },
+    });
+
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: { courseId: params.courseId, isPublished: true },
+    });
+
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: { id: params.courseId },
+        data: { isPublished: true },
+      });
+    }
+
+    return NextResponse.json(deletedChapter);
+  } catch (err) {
+    console.log("[CHAPTER_ID_DELETE]", err);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { courseId: string; chapterId: string } }
 ) {
-
   try {
     const { userId } = auth();
     const { isPublished, ...values } = await req.json();
@@ -33,7 +85,6 @@ export async function PATCH(
       data: { ...values },
     });
 
-    // TODO handle video upload
     if (values.videoUrl) {
       const existingMuxData = await db.muxData.findFirst({
         where: { chapterId: params.chapterId },
