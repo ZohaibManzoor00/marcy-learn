485a446152ea08039571e0442521b4dd43c7c824
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-03-12 18:47:30 -0400

    can view description and title

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { formatPrice } from "@/lib/format";
+
+interface CourseEnrollButtonProps {
+  courseId: string;
+  price: number;
+}
+
+export default function CourseEnrollButton({
+  courseId,
+  price,
+}: CourseEnrollButtonProps) {
+  // const
+  return (
+    <Button className="w-full md:w-auto" size="sm">
+      {formatPrice(price)}
+    </Button>
+  );
+}
