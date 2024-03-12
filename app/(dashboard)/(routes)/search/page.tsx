371e455540f8fe8fe2791f6c625dce463322746f
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-03-11 20:39:59 -0400

    can search and filter for category and course

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,12 +1,18 @@
 import { db } from "@/lib/db";
 import Categories from "./_components/categories";
+import SearchInput from "@/components/search-input";
 
 export default async function Search() {
   const categories = await db.category.findMany({ orderBy: { name: "asc" } });
 
   return (
-    <div className="p-6">
-      <Categories items={categories} />
-    </div>
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
   );
 }
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -5,14 +5,24 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
+import SearchInput from "./search-input";
 
 export default function NavbarRoutes() {
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isPlayerPage = pathname?.includes("/chapter");
+  const isSearchPage = pathname === "/search";
 
   return (
+    <>
+    {isSearchPage && (
+      <div className="hidden md:block">
+        <SearchInput 
+        
+        />
+      </div>
+    )}
     <div className="flex gap-x-2 ml-auto">
       {isTeacherPage || isPlayerPage ? (
         <Link href="/">
@@ -30,5 +40,6 @@ export default function NavbarRoutes() {
       )}
       <UserButton afterSignOutUrl="/" />
     </div>
+    </>
   );
 }
