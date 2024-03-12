10893e8bd055b48cbf7eaece4bd838c7a09b104e
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-03-11 20:23:45 -0400

    toggle category as student

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,3 +1,12 @@
-export default function Search() {
-  return <div>Search page</div>;
+import { db } from "@/lib/db";
+import Categories from "./_components/categories";
+
+export default async function Search() {
+  const categories = await db.category.findMany({ orderBy: { name: "asc" } });
+
+  return (
+    <div className="p-6">
+      <Categories items={categories} />
+    </div>
+  );
 }
