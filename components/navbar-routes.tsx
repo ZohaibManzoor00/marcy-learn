6bd77df2ee2930e0f8fa748300edd808275a3e40
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-03-15 16:53:28 -0400

    teacher auth

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import SearchInput from "./search-input";
+import { isTeacher } from "@/lib/teacher";
 
 export default function NavbarRoutes() {
+  const { userId } = useAuth();
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
@@ -29,13 +31,13 @@ export default function NavbarRoutes() {
               Exit
             </Button>
           </Link>
-        ) : (
+        ) : isTeacher(userId) ? (
           <Link href="/teacher/courses">
             <Button className="sm" variant="ghost">
               Teacher Mode
             </Button>
           </Link>
-        )}
+        ) : null}
         <UserButton afterSignOutUrl="/" />
       </div>
     </>
