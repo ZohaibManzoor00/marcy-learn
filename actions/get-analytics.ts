fcc2082129fc723371f5c55aa03a6170f4bf8262
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-03-15 16:39:25 -0400

    analytics page

---
+++ actions/get-analytics.ts
@@ -0,0 +1,34 @@
+import { db } from "@/lib/db";
+import { Course } from "@prisma/client";
+
+const groupByCourse = (courses: Course[]) => {
+  const grouped: { [courseTitle: string]: number } = {};
+
+  courses.forEach((course) => {
+    const courseTitle = course.title;
+    if (!grouped[courseTitle]) grouped[courseTitle] = 0;
+    grouped[courseTitle] += course.price!;
+  });
+
+  return grouped;
+};
+
+export const getAnalytics = async (userId: string) => {
+  try {
+    const courses = await db.course.findMany({ where: { userId } });
+    const groupedEarnings = groupByCourse(courses);
+    const data = Object.entries(groupedEarnings).map(
+      ([courseTitle, total]) => ({
+        name: courseTitle,
+        total,
+      })
+    );
+    const totalRevenue = data.reduce((acc, cur) => acc + cur.total, 0);
+    const totalSales = courses.length;
+
+    return { data, totalRevenue, totalSales };
+  } catch (err) {
+    console.log("[GET_ANALYTICS]", err);
+    return { data: [], totalRevenue: 0, totalSales: 0 };
+  }
+};
