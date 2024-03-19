98fdcd428a87ff741039a5da310e10bfcda54cee
Zohaib Manzoor <zohaibmanzoor1700@gmail.com>
2024-03-18 22:30:23 -0400

    fixed navbar bg

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -3,7 +3,7 @@ import MobileSidebar from "./mobile-sidebar";
 
 export default function Navbar() {
   return (
-    <div className="p-4 border-b h-full flex items-center shadow-sm">
+    <div className="p-4 border-b h-full flex bg-white dark:bg-background items-center shadow-sm">
       <MobileSidebar />
       <NavbarRoutes />
     </div>
